71fcd537114a649aa9f4fa9b83bcd4efb8a1eaaf
Josue Calvo <josuecalvo@gmail.com>
2013-12-13 11:35:20 +0000

    First version

---
+++ Tables/dbo.Names.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Names]
+(
+[ID] [int] NOT NULL IDENTITY(1, 1),
+[Name] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Names] ADD CONSTRAINT [PK_Names] PRIMARY KEY CLUSTERED  ([ID]) ON [PRIMARY]
+GO
